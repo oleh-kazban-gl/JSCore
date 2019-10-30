ff8530ba90cfc08dac48a4525b56dc2c373d258e
Oleh Kazban <oleh.kazban@globallogic.com>
2019-10-30 20:33:10 +0200

    Added materials for variables

---
+++ src/variables.js
@@ -0,0 +1,35 @@
+// var vs let & const
+
+export function testVar() {
+  var test = 'test var';
+
+  if (test) {
+    var test = 'changed var';
+  }
+
+  console.log('test: ', test);
+}
+
+export function testLet() {
+  let test = 'test let';
+
+  if (test) {
+    let test = 'changed let';
+  }
+
+  test = 'changed again let';
+
+  console.log('test: ', test);
+}
+
+export function testConst() {
+  const test = 'test const';
+
+  if (test) {
+    const test = 'changed const';
+  }
+
+  test = 'changed again const';
+
+  console.log('test: ', test);
+}
